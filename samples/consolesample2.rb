d8d1b3842c63f3f819dd101b0a69b6bb9f26d473
kp-cat <52385411+kp-cat@users.noreply.github.com>
2023-02-14 15:11:39 +0100

    update console samples

--- samples/consolesample2.rb
+++ samples/consolesample2.rb
@@ -5,17 +5,17 @@ require 'configcat'
 ConfigCat.logger.level = Logger::INFO
 
 # Initializing the ConfigCatClient with an SDK Key.
-client = ConfigCat.create_client("PKDVCLf-Hq-h-kCzMp-L7Q/HhOWfwVtZ0mb30i9wi17GQ")
+client = ConfigCat.get("PKDVCLf-Hq-h-kCzMp-L7Q/HhOWfwVtZ0mb30i9wi17GQ")
 
 # Creating a user object to identify your user (optional).
-userObject = ConfigCat::User.new("Some UserID", email: "configcat@example.com", custom: {
+user_object = ConfigCat::User.new("Some UserID", email: "configcat@example.com", custom: {
     'version': '1.0.0'
 })
 
-value = client.get_value("isPOCFeatureEnabled", "default value", userObject)
+value = client.get_value("isPOCFeatureEnabled", "default value", user_object)
 puts("'isPOCFeatureEnabled' value from ConfigCat: " + value.to_s)
 
 value = client.get_value("isAwesomeFeatureEnabled", "default value")
 puts("'isAwesomeFeatureEnabled' value from ConfigCat: " + value.to_s)
 
-client.stop()
+client.close
