b36997a50bcf46cb0f75fc5ae70f46f5256f0be9
Philip Hallstrom <philip@pjkh.com>
2026-09-08 09:14:15 -0700

    Update spec/configcat/configcat_logger_spec.rb
    Co-authored-by: Peter Adam Korodi <52385411+kp-cat@users.noreply.github.com>

--- spec/configcat/configcat_logger_spec.rb
+++ spec/configcat/configcat_logger_spec.rb
@@ -1,21 +1,22 @@
 require 'spec_helper'
 
 RSpec.describe ConfigCat::ConfigCatLogger do
-  let(:subject) { described_class.new(nil) }
+  let(:logger) { double("logger") }
+  let(:configcat_logger) { described_class.new(nil) }
 
   before do
-    ConfigCat.logger = subject
+    allow(ConfigCat).to receive(:logger).and_return(logger)
   end
 
   describe "#enabled_for?" do
     it "works when logger level returns an integer" do
-      allow(subject).to receive(:level).and_return(2)
-      expect(subject.enabled_for?(::Logger::Severity::WARN)).to eq true
+      allow(logger).to receive(:level).and_return(2)
+      expect(configcat_logger.enabled_for?(::Logger::Severity::WARN)).to eq true
     end
 
     it "works when logger level returns a symbol" do
-      allow(subject).to receive(:level).and_return(:warn)
-      expect(subject.enabled_for?(::Logger::Severity::WARN)).to eq true
+      allow(logger).to receive(:level).and_return(:warn)
+      expect(configcat_logger.enabled_for?(::Logger::Severity::WARN)).to eq true
     end
   end
 end
