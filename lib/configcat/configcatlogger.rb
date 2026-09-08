266588ee28406f16febc015e2fb40f4f0f2641c4
Philip Hallstrom <philip@pjkh.com>
2026-09-08 09:13:54 -0700

    Update lib/configcat/configcatlogger.rb
    Co-authored-by: Peter Adam Korodi <52385411+kp-cat@users.noreply.github.com>

--- lib/configcat/configcatlogger.rb
+++ lib/configcat/configcatlogger.rb
@@ -24,5 +24,27 @@ module ConfigCat
       @hooks.invoke_on_error(message)
       ConfigCat.logger.error("[" + event_id.to_s + "] " + message)
     end
+    private
+
+    def coerce_log_level(level)
+      return level if level.is_a?(Integer)
+
+      case level.to_s.downcase
+      when 'trace', 'debug'
+        ::Logger::DEBUG
+      when 'info'
+        ::Logger::INFO
+      when 'warn'
+        ::Logger::WARN
+      when 'error'
+        ::Logger::ERROR
+      when 'fatal'
+        ::Logger::FATAL
+      when 'unknown'
+        ::Logger::UNKNOWN
+      else
+        raise ArgumentError, "invalid log level: #{level}"
+      end
+    end
   end
 end
